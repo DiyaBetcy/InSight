1393922d09e28c9f1c9fe030c217839d37c67dce
Diya Betcy <diyabetcy123@gmail.com>
2025-06-04 12:17:10 +0530

    Added frontend,backend and face recoginition modules

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ app.secret_key = os.environ.get("SESSION_SECRET", "dev-secret-key")
 # Configure the PostgreSQL database
 database_url = os.environ.get("DATABASE_URL")
 if database_url:
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:krtxkrtx@localhost:5432/attendance1'
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///attendance.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///attendance1.db"
     
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_recycle": 300,
